58fda1abdabeb4db68513517a6edfdaf7eecc5df
Jenna Leopold <jleopold424@gmail.com>
2022-03-21 10:21:49 -0400

    completed lesson

--- hello/server.js
+++ hello/server.js
@@ -1,9 +1,37 @@
-const express = require('express');
-const PORT = process.env.PORT || 3000;
+const express = require('express')
+const PORT = process.env.PORT || 3001
 
-const app = express();
+const app = express()
 
+// app.get('/', (req, res) => {
+//   res.send("You're a wizard, Harry Potter!")
+// })
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my webpage')
+})
+
+app.get('/favorite-food', (req, res) => {
+  res.send('Challah')
+})
+
+app.get('/favorite-movie', (req, res) => {
+  res.send('The Wizard of Oz')
+})
+
+app.get('/about-me', (req, res) => {
+  res.send('My name is Jenna')
+})
+
+app.get('/contact', (req, res) => {
+  res.send('My email: jenna.a.leopold@gmail.com')
+})
+
+app.get('/article/:slug', (req, res) => {
+  console.log(req.params)
+  // { slug: 'how-to-improve-your-drumming' }
+})
 
 app.listen(PORT, () => {
-  console.log(`Express server listening on port ${PORT}`);
-});
+  console.log(`Express server listening on port ${PORT}`)
+})
